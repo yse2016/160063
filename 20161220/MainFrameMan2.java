e1aff931ec8cd12aea503a76c7d0ad5317b8884a
shomuyse50 <kitai_shomu@yse-c.net>
2016-12-20 15:58:14 +0900

    MainFrameMan2.java edits

--- 20161220/MainFrameMan2.java
+++ 20161220/MainFrameMan2.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class MainFrameMan2{
@@ -7,20 +8,29 @@ public class MainFrameMan2{
 	}
 }
 
-class FrameMan{
+class FrameMan implements ActionListener{
 		JFrame frame = new JFrame();
 		JLabel label = new JLabel("hello");
 		JButton button = new JButton("Clickme");
+		JTextField text1 = new JTextField(15);
 
-		public FrameMan(){
+
+		public FrameMan(){	
 		frame.setBounds(50,50,300,300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout( new FlowLayout());
 
+		button.addActionListener(this);
+
 		Container ctnr = frame.getContentPane();
-		ctnr.add(label);
+		ctnr.add(text1);
 		ctnr.add(button);
+		ctnr.add(label);
 
 		frame.setVisible(true);
+	}
+	public void actionPerformed(ActionEvent e){
+		String data = text1.getText();
+		label.setText(data);
 	}
 }
